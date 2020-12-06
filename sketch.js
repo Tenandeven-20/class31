c37df4dcbdfcebda90613f3a02e3804d155ea803
Aashni <s166354@student.roundrockisd.org>
2020-12-06 23:45:27 +0000

    we made a new gamestate

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+var array1 = [1,2,3,4,5]
+console.log(array1);
+var array2 = [100,"name",true,null]
+console.log(array2);
+var array3 = [[1,2],[3,4]]
+console.log(array3);
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -7,7 +14,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var gameState = "onSling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,16 +76,19 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gameState !== "launched"){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched";
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+        //slingshot.attach(bird.body);
     }
 }
